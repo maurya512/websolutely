cbfaf1624888a7906b84c2e99ef62ec6e3f0f0f3
maurya512 <patelmaurya0512@gmail.com>
2021-06-23 22:40:33 -0400

    json data for services

---
+++ src/components/Data.js
@@ -0,0 +1,22 @@
+// the file where data for all services provided can be found
+
+// import images to use for the data 
+import websrvc from '../images/bg.png'
+
+// json type data files used to render info to the card
+// array of objects
+
+const Data = [
+    {
+        imgsrc: websrvc,
+        title: "Web Development",
+        description: "Build, Deploy and Maintain world class web applications.",
+    },
+    {
+        imgsrc: websrvc, 
+        title: "Product Manager",
+        description: "Managing your projects "
+    }
+]
+
+export default Data
