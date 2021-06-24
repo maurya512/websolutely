c45adc07a0c5b2b999fa75ac815414f7bd472ddc
maurya512 <patelmaurya0512@gmail.com>
2021-06-23 22:41:21 -0400

    imported the card and data components

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,10 +1,37 @@
+// import dependencies
 import React from 'react'
 
+
+// import files
+import Card from './Card';
+import Data from './Data';
+
+// functional component
 function Service() {
     return (
-        <h1>
-            Welcome to the Service page
-        </h1>
+        <>
+            <div className="my-5">
+                <h1 className="text-center">Service</h1>
+            </div>
+            <div className="container-fluid mb-5">
+                <div className="row">
+                    <div className="col-10 mx-auto">
+                        <div className="row gy-4">
+                            {/* mapping through the array of objects in the data component */}
+                            {
+                                Data.map((val, ind) => {
+                                    return <Card key={ind}
+                                    imgsrc={val.imgsrc}
+                                    title={val.title}
+                                    description={val.description}
+                                    />
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
